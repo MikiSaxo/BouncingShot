12f499f3974fd0982c773dd2edc5248b93336469
Samuel <samuel.charlet62@gmail.com>
2022-09-11 17:03:45 +0200

    Nouveau Menu (pas relié avec l'autre scene encore)

--- Assets/_Scripts/Manage/GameParameters.cs
+++ Assets/_Scripts/Manage/GameParameters.cs
@@ -2,10 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
 
 public class GameParameters : MonoBehaviour
 {
     public WhichMode Mode;
+    public int NbOfPlayers;
+    public int RedColors;
+    public int BlueColors;
+    public Color[] blueColorToChoose;
+    public Color[] redColorToChoose;
+
+    public enum WhichMode
+    {
+        Normal,
+        Blitz,
+        Domination,
+        Possession,
+        Soccer
+    }
 
     public static GameParameters instance;
     private void Awake()
@@ -19,20 +35,39 @@ public class GameParameters : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    public enum WhichMode
+    private void Start()
     {
-        Normal,
-        Blitz,
-        Domination,
-        Possession,
-        Soccer
+        for (int i = 0; i < blueColorToChoose.Length; i++)
+        {
+            MenuSelection.Instance.ChangeBlueColor(i, blueColorToChoose[i]);
+        }
+        for (int i = 0; i < redColorToChoose.Length; i++)
+        {
+            MenuSelection.Instance.ChangeRedColor(i, redColorToChoose[i]);
+        }
     }
 
     public void ChooseMode(int mode)
     {
         Mode = (WhichMode)mode;
         print(Mode);
-        SceneManager.LoadScene(1);
+        MenuSelection.Instance.SelectNbPlayers();
+        //SceneManager.LoadScene(1);
     }
 
+    public void ChooseNbPlayers(int howMany)
+    {
+        NbOfPlayers = howMany;
+        print(NbOfPlayers);
+    }
+
+    public void ChooseBlueColor(int whichColor)
+    {
+        BlueColors = whichColor;
+    }
+
+    public void ChooseRedColor(int whichColor)
+    {
+        RedColors = whichColor;
+    }
 }
--- Assets/_Scripts/UI/MenuSelection.cs
+++ Assets/_Scripts/UI/MenuSelection.cs
@@ -1,20 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 using TMPro;
+using DG.Tweening;
 
 public class MenuSelection : MonoBehaviour
 {
-    [SerializeField] GameObject chooseFirstButton, currentSelected;
+    [SerializeField] GameObject currentSelected;
+    [SerializeField] GameObject[] chooseFirstButtons;
     [SerializeField] GameObject[] Buttons;
+    [SerializeField] Transform[] tpPoints;
+    [SerializeField] private GameObject menus = null;
     [SerializeField] string[] descriptions;
     [SerializeField] TextMeshProUGUI descriptionObject;
+    [SerializeField] GameObject[] blueColors;
+    [SerializeField] GameObject[] redColors;
+    [SerializeField] GameObject[] validateIconsColor;
+    [SerializeField] GameObject followIconColor;
+
+    private int currentPoint = 0;
+
+    public static MenuSelection Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        menus.transform.DOMoveX(tpPoints[0].position.x, 1f);
+    }
+
+    public void ChangeBlueColor(int index, Color color)
+    {
+        blueColors[index].GetComponent<Image>().color = color;
+    }
+    public void ChangeRedColor(int index, Color color)
+    {
+        redColors[index].GetComponent<Image>().color = color;
+    }
 
     private void Update()
     {
         currentSelected = EventSystem.current.currentSelectedGameObject;
+
+        if (currentPoint == 3)
+            followIconColor.transform.position = currentSelected.transform.position;
+        else
+            followIconColor.transform.position = new Vector3(3000, 3000, 0);
+
         if (currentSelected != null)
             ChangeText();
     }
@@ -23,12 +60,44 @@ public class MenuSelection : MonoBehaviour
     {
         //SceneManager.LoadScene(1);
         EventSystem.current.SetSelectedGameObject(null);
-        EventSystem.current.SetSelectedGameObject(chooseFirstButton);
+        EventSystem.current.SetSelectedGameObject(chooseFirstButtons[0]);
+    }
+
+    public void SelectNbPlayers()
+    {
+        EventSystem.current.SetSelectedGameObject(chooseFirstButtons[1]);
+    }
+
+    public void ChooseNbPlayers()
+    {
+        EventSystem.current.SetSelectedGameObject(chooseFirstButtons[2]);
     }
 
     public void OnQuit()
     {
         Application.Quit();
+    }
+
+    public void MoveToLeftMenus()
+    {
+        currentPoint++;
+        menus.transform.DOMoveX(tpPoints[currentPoint].position.x, 1f);
+    }
+
+    public void MoveToRightMenus()
+    {
+        currentPoint--;
+        menus.transform.DOMoveX(tpPoints[currentPoint].position.x, 1f);
+    }
+
+    public void ValidateBlueSide()
+    {
+        validateIconsColor[0].transform.position = followIconColor.transform.position;
+    }
+
+    public void ValidateRedSide()
+    {
+        validateIconsColor[1].transform.position = followIconColor.transform.position;
     }
 
     void ChangeText()
