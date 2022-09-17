ba15d15176ad41e5ee82c63d315676a43c56fd85
Samuel <samuel.charlet62@gmail.com>
2022-09-17 12:46:53 +0200

    FB recul

--- Assets/_Scripts/Gameplay/CursorMovement.cs
+++ Assets/_Scripts/Gameplay/CursorMovement.cs
@@ -17,8 +17,12 @@ public class CursorMovement : MonoBehaviour
     [SerializeField] GameObject Bullet, Cursor, SpawnBullet;
     [SerializeField] GameObject[] IndicatorsCanShoot;
     [SerializeField] float timer;
-    [SerializeField] GameObject canShootDetector;
+    //[SerializeField] GameObject canShootDetector;
+    [SerializeField] GameObject normalScaleSprite;
+    private Vector3 getNormalSpritePos;
     private bool canShoot = false;
+    private float facingAngle = 0;
+    private bool canResetNormalSprPos = false;
 
     private void Start()
     {
@@ -51,9 +55,9 @@ public class CursorMovement : MonoBehaviour
         if (isCooldown)
         {
             nextAttack -= Time.deltaTime;
-            if (nextAttack <= attackRate/2)
+            if (nextAttack <= attackRate / 2)
                 IndicatorsCanShoot[0].SetActive(true);
-                IndicatorsCanShoot[0].GetComponent<ReboundAnimation>().StartBounce();
+            IndicatorsCanShoot[0].GetComponent<ReboundAnimation>().StartBounce();
 
             if (nextAttack <= 0)
             {
@@ -62,6 +66,11 @@ public class CursorMovement : MonoBehaviour
                 IndicatorsCanShoot[1].GetComponent<ReboundAnimation>().StartBounce();
             }
         }
+
+        //if (normalScaleSprite)
+        //{
+        normalScaleSprite.transform.position = Vector3.Lerp(normalScaleSprite.transform.position, transform.position, Time.deltaTime * 5);
+        //}
     }
 
     public void OnRotate(InputAction.CallbackContext context)
@@ -83,12 +92,13 @@ public class CursorMovement : MonoBehaviour
     {
         canShoot = which;
     }
-     
+
     void Rotate()
     {
         if (!IsLock)
         {
             float angle = Mathf.Atan2(movementInputRotate.y, movementInputRotate.x) * Mathf.Rad2Deg;
+            facingAngle = angle;
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
         }
     }
@@ -97,6 +107,7 @@ public class CursorMovement : MonoBehaviour
     {
         Vector2 direction = Manager.instance.Ball.transform.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        facingAngle = angle;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 50 * Time.deltaTime);
     }
@@ -110,12 +121,13 @@ public class CursorMovement : MonoBehaviour
     void Shoot()
     {
         gameObject.GetComponent<ReboundAnimation>().StartBounce();
+        MoveBackfeedback();
         transferPosition = new Vector3(SpawnBullet.transform.position.x, SpawnBullet.transform.position.y, 0);
         GameObject b = Instantiate(Bullet, transferPosition, Cursor.transform.rotation);
-        
+
         if (GetComponent<WhoAreYou>().ChoisiBieng == WhoAreYou.ChooseYourChampion.P2)
             b.gameObject.GetComponent<WhoAreYou>().ChoisiBieng = WhoAreYou.ChooseYourChampion.BulletP2;
-        
+
         if (WhichPlayer == 1)
         {
             b.GetComponentInChildren<SpriteRenderer>().color = Manager.instance.statesColor[1];
@@ -126,5 +138,13 @@ public class CursorMovement : MonoBehaviour
             b.GetComponentInChildren<SpriteRenderer>().color = Manager.instance.statesColor[2];
             b.GetComponentInChildren<TrailRenderer>().startColor = Manager.instance.statesColor[2];
         }
+    }
+
+    void MoveBackfeedback()
+    {
+        getNormalSpritePos = normalScaleSprite.transform.position;
+        
+        Vector2 finalMovement = new Vector2(Mathf.Cos(facingAngle * Mathf.Deg2Rad), Mathf.Sin(facingAngle * Mathf.Deg2Rad));
+        normalScaleSprite.transform.DOMove(new Vector3(getNormalSpritePos.x + finalMovement.x * -.2f, getNormalSpritePos.y + finalMovement.y * -.2f, 0), 0f);
     }
 }
