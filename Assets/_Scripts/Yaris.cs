b1715d103f30b159b5ebbeb0a5f22f5da8d443d8
Samuel <samuel.charlet62@gmail.com>
2022-10-28 20:53:08 +0200

    Fix

--- Assets/_Scripts/Yaris.cs
+++ Assets/_Scripts/Yaris.cs
@@ -5,12 +5,7 @@ using DG.Tweening;
 
 public class Yaris : MonoBehaviour
 {
-    [SerializeField] private float speed = 0f;
     [SerializeField] private Rigidbody2D rbBall;
-    //void Start()
-    //{
-    //    transform.DORotate(new Vector3(0, 0, 360), speed, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
-    //}
 
     private void Update()
     {
